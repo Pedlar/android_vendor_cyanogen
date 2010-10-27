58e802464f5ed49d5bd047c99f479967c507ea2b
Wes Garner <wesgarner@gmail.com>
2010-10-27 01:40:55 -0500

    Captivate: Add SGH-I897 from atinm
    Change-Id: I08eb3046fe4795358ee41fa567962089e30325c6

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,5 +13,6 @@ add_lunch_combo cyanogen_vibrant-eng
 add_lunch_combo cyanogen_legend-eng
 add_lunch_combo cyanogen_vision-eng
 add_lunch_combo cyanogen_epic-eng
+add_lunch_combo cyanogen_captivate-eng
 
 PATH=$PATH:$PWD/vendor/cyanogen/tools ; export PATH
